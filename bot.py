86dd0d67ca375c2fd1911a0340f91d219ede7039
Skerpsh <kocsvyatoslav988@gmail.com>
2026-01-23 21:37:37 +0000

    Initial commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
